d56e3dbb2ba9a059a38d8692d506dc58906c10ea
Ann Lyapunova <lord.letopisniy@yandex.ru>
2024-09-01 13:55:21 +0300

    create tests functions for func get_date and tests passed

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -6,3 +6,7 @@ from src.widget import mask_account_card, get_date
 def test_mask_account_card(entered, obtained):
     assert mask_account_card(entered) == obtained
 
+
+@pytest.mark.parametrize("old_date, new_date", [("2024-03-11T02:26:18.671407", "11.03.2024"), ("2024-08-14T02:26:18.671407", "14.08.2024")])
+def test_get_date(old_date, new_date):
+    assert get_date(old_date) == new_date

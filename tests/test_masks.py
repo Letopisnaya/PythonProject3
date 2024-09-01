ebc62fb879ecfcf34235cfe25b2455a546059534
Ann Lyapunova <lord.letopisniy@yandex.ru>
2024-09-01 13:22:45 +0300

    tests passed

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -3,9 +3,9 @@ from src.masks import get_mask_account, get_mask_card_number
 
 
 def test_get_mask_card():
-    assert get_mask_card_number("7000792289606361") == "700079******6361"
+    assert get_mask_card_number("7000792289606361") == "7000 79** **** 6361"
 
 def test_get_mask_account():
-    assert get_mask_account("73654108430135874305") == "**4305"
+    assert get_mask_account("73654108430135874305") == "** 4305"
 
 

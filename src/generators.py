7a0d495dd26c63096e09c21a9db299640478fafe
Ann Lyapunova <lord.letopisniy@yandex.ru>
2024-09-07 12:23:39 +0300

    added generator func that generate card number

--- src/generators.py
+++ src/generators.py
@@ -19,3 +19,10 @@ def transaction_descriptions(transaction_list):
         yield transaction
 
 
+def card_number_generator(start: int, end: int) -> str:
+    for number in range(start, end+1):
+        card_number = str(number)
+        while len(card_number) < 16:
+            card_number = '0' + card_number
+        formatted_card_number = f"{card_number[:4]} {card_number[4:8]} {card_number[8:12]} {card_number[12:]}"
+    	yield (formatted_card_number)

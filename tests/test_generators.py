91ab288b2e11f6f07bf323b46875353774575a8f
Ann Lyapunova <lord.letopisniy@yandex.ru>
2024-09-07 14:46:53 +0300

    add last test for last function in generators.py

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -124,3 +124,10 @@ def test_transaction_descriptions(transaction_list):
     assert next(des) == "Перевод со счета на счет"
     assert next(des) == "Перевод со счета на счет"
     assert next(des) == "Перевод с карты на карту"
+
+
+def test_card_number_generator():
+    gen_number = card_number_generator(1, 3)
+    assert next(gen_number) == "0000 0000 0000 0001"
+    assert next(gen_number) == "0000 0000 0000 0002"
+    assert next(gen_number) == "0000 0000 0000 0003"

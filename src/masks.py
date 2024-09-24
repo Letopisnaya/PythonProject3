01ee0113bcdef4d82e08cb2af71ffd96c6696dce
Ann Lyapunova <lord.letopisniy@yandex.ru>
2024-09-24 18:05:25 +0300

    create logges for module utils and log files

--- src/masks.py
+++ src/masks.py
@@ -1,3 +1,5 @@
+
+
 def get_mask_card_number(card_number: str) -> str:
     """Функция скрытия номера карты"""
     return f"{card_number[:4]} {card_number[4:6]}{"*" * 2} {"*" * 4} {card_number[-4:]}"

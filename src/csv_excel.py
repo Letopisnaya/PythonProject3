676c97207fde531120244b9f51ba06f6fa676499
Ann Lyapunova <lord.letopisniy@yandex.ru>
2024-09-28 19:25:16 +0300

    Bugs fixed

--- src/csv_excel.py
+++ src/csv_excel.py
@@ -1,17 +1,30 @@
 from pathlib import Path
-
 import pandas as pd
+import csv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 join_path_csv = BASE_DIR / "data" / "transactions.csv"
 join_path_exc = BASE_DIR / "data" / "transactions_excel.xlsx"
 
+# with open('transactions.csv') as file:
+#   reader = csv.DictReader(file, delimiter=';')
+#  for row in reader:
+#     print(row)
+
 
 def transaction_read_csv(join_path_csv):
     """Функция принимает на вход путь до файла csv и возвращает список словарей с транзакциями"""
     try:
-        transaction_csv = pd.read_csv(join_path_csv)
-        return transaction_csv
+        with open(join_path_csv, "r", encoding="utf-8") as file:
+            reader = csv.reader(file, delimiter=";")
+            header = next(reader)
+            result = []
+            for row in reader:
+                row_dict = dict()
+                for idx, item in enumerate(header):
+                    row_dict[item] = row[idx]
+                result.append(row_dict)
+        return result
     except FileNotFoundError:
         transactions = []
         return transactions
@@ -25,6 +38,7 @@ def transaction_read_exsel(join_path_exc):
     except FileNotFoundError:
         transactions = []
         return transactions
+
 
 print(transaction_read_csv(join_path_csv))
 print(transaction_read_exsel(join_path_exc))
